c32c3fb83e08a3597324f673c8428a4dce460845
EVAN NGUYEN <101128471+Evannguyen273@users.noreply.github.com>
2024-11-09 15:09:03 -0500

    Create plot_config.py

---
+++ plot_config.py
@@ -0,0 +1,17 @@
+mpl.style.use("ggplot")
+mpl.rcParams["figure.figsize"] = (20, 5)
+mpl.rcParams["axes.facecolor"] = "white"
+mpl.rcParams["axes.grid"] = True
+mpl.rcParams["grid.color"] = "lightgray"
+mpl.rcParams["axes.prop_cycle"] = colors
+mpl.rcParams["axes.linewidth"] = 1
+mpl.rcParams["xtick.color"] = "black"
+mpl.rcParams["ytick.color"] = "black"
+mpl.rcParams["font.size"] = 13
+mpl.rcParams["figure.titlesize"] = 20
+mpl.rcParams["figure.dpi"] = 100
+mpl.rcParams["legend.fontsize"] = 10
+mpl.rcParams['figure.dpi'] = 100
+mpl.rcParams['xtick.labelsize'] = 10
+mpl.rcParams['ytick.labelsize'] = 10
+mpl.rcParams['axes.labelsize'] = 12
